a771b5841321f09c9c311a23e8905a3e79cceb20
asadbek <adizjonovasadulloh97@gmail.com>
2023-07-24 09:57:27 +0500

    Initial commit

---
+++ src/main/java/com/example/gitlesson/GitLessonApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitlesson/GitLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitlesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
